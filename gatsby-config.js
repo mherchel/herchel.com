112fb9bed5f92f9a7b34f37d13ff645d671c6cc2
Mike Herchel <mike@herchel.com>
2018-01-01 18:59:47 -0500

    Renaming site from default value.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter Blog",
-    author: "Kyle Mathews",
+    title: "Mike Herchel | 8th Grade Geography Bee Champion",
+    author: "Mike Herchel",
   },
   plugins: [
     {
