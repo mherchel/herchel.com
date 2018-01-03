719b1bd6625a969eeb24545b083fe73d4bb982bb
Mike Herchel <mike@herchel.com>
2018-01-03 15:59:10 -0500

    Fixing links on contact page.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,19 +8,19 @@ export default ({ data }) =>
     <Helmet title={`${pageName} | ${data.site.siteMetadata.title}`} />
     <h1>{pageName}</h1>
     <h2>Email Addy</h2>
-    <p>Email me at <a href="https://web.archive.org/web/20160820020817/mailto:mike@herchel.com" target="_blank">mike@herchel.com</a></p>
+    <p>Email me at <a href="mailto:mike@herchel.com" target="_blank">mike@herchel.com</a></p>
     <h2>Tweet me on Twitter</h2>
-    <p>Follow me at <a href="https://web.archive.org/web/20160820020817/http://twitter.com/mikeherchel" target="_blank">@mikeherchel</a></p>
+    <p>Follow me at <a href="http://twitter.com/mikeherchel" target="_blank">@mikeherchel</a></p>
     <h2>Find out where I'll be speaking next</h2>
-    <p>I'm on Lanyrd at <a href="https://web.archive.org/web/20160820020817/http://lanyrd.com/profile/mherchel/" target="_blank">lanyrd.com/profile/mherchel/</a></p>
+    <p>I'm on Lanyrd at <a href="http://lanyrd.com/profile/mherchel/" target="_blank">lanyrd.com/profile/mherchel/</a></p>
     <h2>Git me on Github</h2>
-    <p>I'm on github at <a href="https://web.archive.org/web/20160820020817/https://github.com/mherchel" target="_blank">github.com/mherchel</a></p>
+    <p>I'm on github at <a href="https://github.com/mherchel" target="_blank">github.com/mherchel</a></p>
     <h2>Friend me on Facebook</h2>
-    <p>Facebook profile is at <a href="https://web.archive.org/web/20160820020817/http://www.facebook.com/mherchel" target="_blank">facebook.com/mherchel</a></p>
+    <p>Facebook profile is at <a href="http://www.facebook.com/mherchel" target="_blank">facebook.com/mherchel</a></p>
     <h2>Drupal.org Profile</h2>
-    <p>Find me on D.O at <a href="https://web.archive.org/web/20160820020817/http://drupal.org/user/118428" target="_blank">drupal.org/user/118428</a></p>
+    <p>Find me on D.O at <a href="http://drupal.org/user/118428" target="_blank">drupal.org/user/118428</a></p>
     <h2>I don't use LinkedIn</h2>
-    <p>But when I do, I'm at <a href="https://web.archive.org/web/20160820020817/http://www.linkedin.com/in/mherchel" target="_blank">linkedin.com/in/mherchel</a></p>
+    <p>But when I do, I'm at <a href="http://www.linkedin.com/in/mherchel" target="_blank">linkedin.com/in/mherchel</a></p>
   </div>
 
 export const query = graphql`
