7b89c54ac5374023c48458ae81374d8e8082f24b
Mike Herchel <mike@herchel.com>
2018-01-06 12:48:30 -0500

    Revert "Add blog/ prefix onto blog post paths."
    This reverts commit b7b3a79f852d57eb35915ecdd8f848ca8a2effef.

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,11 +8,11 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
   const { createNodeField } = boundActionCreators
   if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `blog` })
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
     createNodeField({
       node,
       name: `slug`,
-      value: `blog${slug}`,
+      value: slug,
     })
   }
 }
