b7b3a79f852d57eb35915ecdd8f848ca8a2effef
Mike Herchel <mike@herchel.com>
2018-01-06 10:57:59 -0500

    Add blog/ prefix onto blog post paths.

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,11 +8,11 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
   const { createNodeField } = boundActionCreators
   if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    const slug = createFilePath({ node, getNode, basePath: `blog` })
     createNodeField({
       node,
       name: `slug`,
-      value: slug,
+      value: `blog${slug}`,
     })
   }
 }
