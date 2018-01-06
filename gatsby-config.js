8964605c4034b142a80d48a591d192ba76212e30
Mike Herchel <mike@herchel.com>
2018-01-06 12:50:21 -0500

    Make sure gatsby can find new blogs.

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        path: `${__dirname}/src/blog`,
+        name: "blog",
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         path: `${__dirname}/src/data`,
         name: "data",
       },
