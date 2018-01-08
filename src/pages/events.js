8fdcafff9c9235996d92734f5aaed5341de4ec17
Mike Herchel <mike@herchel.com>
2018-01-08 15:30:00 -0500

    Splitting past and future events

--- src/pages/events.js
+++ src/pages/events.js
@@ -9,6 +9,15 @@ class EventIndex extends React.Component {
   render() {
     const siteTitle = `${get(this, 'props.data.site.siteMetadata.title')} | ${pageName}`
     const events = get(this, 'props.data.allEventsJson.edges')
+    const now = new Date(Date(now)).getTime()
+    const prevEvents = events.filter(event => {
+      const eventDate = new Date(event.node.date).getTime()
+      return eventDate < now
+    })
+    const upcomingEvents = events.filter(event => {
+      const eventDate = new Date(event.node.date).getTime()
+      return eventDate > now
+    })
 
     return (
       <div className='content'>
@@ -17,9 +26,27 @@ class EventIndex extends React.Component {
           <meta name="description" content={`Events and conferences that is going to, and has been to.`} />
         </Helmet>
         <h1>{pageName}</h1>
-
-        <div className="events">
-          {events.map(event => {
+        <h2>Upcoming events</h2>
+        <div className="events upcoming">
+          {upcomingEvents.map(event => {
+            const title = get(event, 'node.title')
+            const date = get(event, 'node.date')
+            const status = get(event, 'node.status')
+            const location = get(event, 'node.location')
+            const url = get(event, 'node.url')
+            return (
+              <div key={date}>
+                <h3><a target="_blank" href={url}>{title}</a></h3>
+                <div className="date">{date}</div>
+                <div className="status">{status}</div>
+                <div className="location">{location}</div>
+              </div>
+            )
+          })}
+        </div>
+        <h2>Previous events</h2>
+        <div className="events previous">
+          {prevEvents.map(event => {
             const title = get(event, 'node.title')
             const date = get(event, 'node.date')
             const status = get(event, 'node.status')
@@ -53,7 +80,7 @@ export const eventQuery = graphql`
         title
       }
     }
-    allEventsJson(sort: { fields: [date], order: ASC }) {
+    allEventsJson(sort: { fields: [date], order: DESC }) {
       edges {
         node {
           title
