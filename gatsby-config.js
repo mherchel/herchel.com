61a18f944fe796cd61a534d012c641ebbb018856
Mike Herchel <mike@herchel.com>
2018-01-20 19:37:18 -0500

    Adding GA tracking info.

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-8174073-1`,
       },
     },
     `gatsby-plugin-offline`,
