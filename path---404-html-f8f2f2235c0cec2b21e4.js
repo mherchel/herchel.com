a1c14445cf2f16e30e89029da9cbb8192afb91e6
Mike Herchel <mike@herchel.com>
2018-01-27 20:05:57 -0500

    Updates

--- path---404-html-f8f2f2235c0cec2b21e4.js
+++ path---404-html-f8f2f2235c0cec2b21e4.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xa2868bfb69fc],{403:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Mike Herchel"}}},pathContext:{}}}});
+webpackJsonp([0xa2868bfb69fc],{404:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Mike Herchel"}}},pathContext:{}}}});
 //# sourceMappingURL=path---404-html-f8f2f2235c0cec2b21e4.js.map
