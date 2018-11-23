49705e329fc77379910ff56a3839768eb700477b
Mike Herchel <mike@lullabot.com>
2018-11-23 09:49:10 -0500

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-8ad35cd93a33e2d41932.js","982ec61c4c9d5580b158e4d69c454236"],["/app-98b304c66862370226ea.js","bcd22d0906737fbe7df36d959d6b72e8"],["/app-caadc7eb004207f5a547.js","6492eed893f3993f1ce89474b0e939a5"],["/commons-4890461d6a37e9fbc929.js","84cba4c4de61cc624f018f67bb6867a6"],["/index.html","a25932480915b2f3190868fed084a678"],["/offline-plugin-app-shell-fallback/index.html","035d87723bc1a5a9e94678f67e1f37b8"]];
+var precacheConfig = [["/app-8ad35cd93a33e2d41932.js","982ec61c4c9d5580b158e4d69c454236"],["/app-98b304c66862370226ea.js","bcd22d0906737fbe7df36d959d6b72e8"],["/app-caadc7eb004207f5a547.js","6492eed893f3993f1ce89474b0e939a5"],["/commons-4890461d6a37e9fbc929.js","84cba4c4de61cc624f018f67bb6867a6"],["/index.html","c575e2d433dab694d7657799d6b76c68"],["/offline-plugin-app-shell-fallback/index.html","b76403c84fe19b03358e1e3e453ed0ef"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
